dbce65bff6489b6d315f66459dc05f43cbba81d6
Aftab Hussain <aftabh4004@gmail.com>
2021-02-18 12:40:35 +0530

    sidebar is replaced by responsive nav bar, body section is also responsive now

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -3,26 +3,42 @@ import "./sidebar.css";
 
 const SideBar = ({ getRecipe, setShowCover, setShowLoading }) => {
   const handleCatClick = (event) => {
+    document
+      .getElementsByClassName("sidebar__links")[0]
+      .classList.toggle("active");
     setShowLoading(true);
     setShowCover(false);
     const category = event.target.id;
     getRecipe(category);
   };
+
+  const handleToggleClick = () => {
+    document
+      .getElementsByClassName("sidebar__links")[0]
+      .classList.toggle("active");
+  };
   return (
     <div className="sidebar">
-      <h1 className="sidebar__header">Category</h1>
-      <h3 className="sidebar__cat" id="Chicken" onClick={handleCatClick}>
-        Chicken
-      </h3>
-      <h3 className="sidebar__cat" id="Mutton" onClick={handleCatClick}>
-        Mutton
-      </h3>
-      <h3 className="sidebar__cat" id="Paneer" onClick={handleCatClick}>
-        Paneer
-      </h3>
-      <h3 className="sidebar__cat" id="Vegetables" onClick={handleCatClick}>
-        Vegetables
-      </h3>
+      <h1 className="sidebar__header">foodani</h1>
+      <a href="#" className="toggle__button" onClick={handleToggleClick}>
+        <span className="toggle__bar"></span>
+        <span className="toggle__bar"></span>
+        <span className="toggle__bar"></span>
+      </a>
+      <div className="sidebar__links">
+        <h3 className="sidebar__cat" id="Chicken" onClick={handleCatClick}>
+          Chicken
+        </h3>
+        <h3 className="sidebar__cat" id="Mutton" onClick={handleCatClick}>
+          Mutton
+        </h3>
+        <h3 className="sidebar__cat" id="Paneer" onClick={handleCatClick}>
+          Paneer
+        </h3>
+        <h3 className="sidebar__cat" id="Vegetables" onClick={handleCatClick}>
+          Vegetables
+        </h3>
+      </div>
     </div>
   );
 };
